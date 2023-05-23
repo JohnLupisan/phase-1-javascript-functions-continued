37c8bf43de180bd422907a513249b091e1363246
John Eric Lupisan <john.lupisan@gmail.com>
2023-05-22 18:32:27 -0800

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+   return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office') {
+   return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(string='*') {
+   const param1 = "You are"
+   return function (param2 = "special")
+{
+   return `${param1} ${string}${param2}${string}!`;
+}
+}
